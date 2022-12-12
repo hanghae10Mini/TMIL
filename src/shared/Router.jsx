63d66f5ac31c105caa6ae3b8fae185753acb659f
Jdentity <identityjang@gmail.com>
2022-12-12 13:51:41 +0900

    Add  router pages
    Home, Create, Update, Details 페이지 추가

---
+++ src/shared/Router.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Create from '../pages/Create';
+import Details from '../pages/Details';
+import Home from '../pages/Home';
+import Update from '../pages/Update';
+
+function Router() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/create" element={<Create />} />
+        <Route path="/update" element={<Update />} />
+        <Route path="details" element={<Details />} />
+        {/* <Route path="details/:id" element={<Details />} /> */}
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default Router;
