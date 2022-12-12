82a588ebfba170509f84c2d19db8838f8a5bc245
GosuEE <leemin727@naver.com>
2022-12-12 22:29:32 +0900

    Slice, JSON server 코딩중

---
+++ src/redux/configure/modules/postSlice.jsx
@@ -0,0 +1,77 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+const initialState = {
+  posts: [],
+  post: {
+    title: '',
+    contents: '',
+    name: '',
+    password: '',
+    postId: 0,
+  },
+  isLoading: false,
+  error: null,
+};
+
+export const createPost = createAsyncThunk('post/CREATE_POST', async (payload, thunkAPI) => {
+  try {
+    await axios.post('http://localhost:3001/post', payload);
+    return '게시글 작성 완료';
+  } catch (error) {
+    return thunkAPI.rejectWithValue(error);
+  }
+});
+
+export const readPost = createAsyncThunk('post/READ_POST', async (payload, thunkAPI) => {
+  try {
+    const data = await axios.get('http://localhost:3001/posts');
+    return thunkAPI.fulfillWithValue(data.data);
+  } catch (error) {
+    return thunkAPI.rejectWithValue(error);
+  }
+});
+
+export const updatePost = createAsyncThunk('post/UPDATE_POST', async (payload, thunkAPI) => {
+  try {
+    const data = await axios.patch(`http://localhost:3001/post/${payload.id}`, {
+      title: payload.title,
+      contents: payload.contetns,
+    });
+    return thunkAPI.fulfillWithValue(data.data);
+  } catch (error) {
+    return thunkAPI.rejectWithValue(error);
+  }
+});
+
+export const deletePost = createAsyncThunk('post/DELETE_POST', async (payload, thunkAPI) => {});
+
+const postSlice = createSlice({
+  name: 'post',
+  initialState,
+  reducers: {},
+  extraReducers: {
+    [createPost.pending]: (state) => {
+      state.isLoading = true;
+    },
+    [createPost.fulfilled]: (state) => {
+      state.isLoading = false;
+    },
+    [createPost.rejected]: (state, action) => {
+      state.isLoading = false;
+      state.error = action.payload;
+    },
+
+    [readPost.pending]: (state) => {
+      state.isLoading = true;
+    },
+    [readPost.fulfilled]: (state, action) => {
+      state.isLoading = false;
+      state.posts = action.payload;
+    },
+    [readPost.rejected]: (state, action) => {
+      state.isLoading = false;
+      state.error = action.payload;
+    },
+  },
+});
