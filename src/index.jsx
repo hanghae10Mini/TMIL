75e2ad5ba070923d9debf7ffb1ce2e2e88cbb0e5
Jdentity <identityjang@gmail.com>
2022-12-13 14:20:23 +0900

    Fix simple things
    빈 Header 파일 삭제
    redux 폴더 내부 빈 파일 삭제
    combineReudcer가 빈객체를 받아 오류가 나서 주석처리
    store Provider도 함께 주석처리

--- src/index.jsx
+++ src/index.jsx
@@ -4,17 +4,17 @@ import { Provider } from 'react-redux';
 import { ThemeProvider } from '@mui/material';
 import App from './App';
 import GlobalStyle from './styles/GlobalStyles';
-import store from './redux/configure/configStore';
+// import store from './redux/configure/configStore';
 import theme from './styles/theme';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <ThemeProvider theme={theme}>
-        <GlobalStyle />
-        <App />
-      </ThemeProvider>
-    </Provider>
+    {/* <Provider store={store}> */}
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <App />
+    </ThemeProvider>
+    {/* </Provider> */}
   </React.StrictMode>,
 );
