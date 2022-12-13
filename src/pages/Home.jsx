e4559ed6bc15bd6db8cfe089c3cd8b25bbbc78ee
ssori <115159126+ssori0421@users.noreply.github.com>
2022-12-13 09:28:28 +0900

    주석 삭제

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,6 @@ import { Box, Stack, Paper, Typography } from '@mui/material';
 import styled from 'styled-components';
 import Card from '../components/card/Card';
 
-// Board 필요한 데이터 =>
 export default function Home() {
   return (
     <Stack
@@ -25,57 +24,6 @@ export default function Home() {
         <Stack spacing={2}>
           {/* CardItem => 컴포넌트, props ={ title, content, day(format: yyyy/mm/dd) } */}
           <Card title="[TIL]오늘 잠만 잤어요" content="내용" day="2022/10/22" />
-          {/* <Box
-            width="100%"
-            py={2}
-            bgcolor="#F9FCFD"
-            borderRadius={2}
-            sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}
-          >
-            <Typography pl={2} sx={{ width: '33%' }} variant="h6">
-              [TIL]오늘 잠만 잤어요
-            </Typography>
-            <Typography sx={{ width: '33%', textAlign: 'center' }} variant="h6">
-              잠만보
-            </Typography>
-            <Typography sx={{ width: '33%', textAlign: 'center' }} variant="h6">
-              2023/2/21
-            </Typography>
-          </Box>
-          <Box
-            width="100%"
-            py={2}
-            bgcolor="#F9FCFD"
-            borderRadius={2}
-            sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}
-          >
-            <Typography pl={2} sx={{ width: '33%' }} variant="h6">
-              [TIL]오늘 잠만 잤어요
-            </Typography>
-            <Typography sx={{ width: '33%', textAlign: 'center' }} variant="h6">
-              잠만보
-            </Typography>
-            <Typography sx={{ width: '33%', textAlign: 'center' }} variant="h6">
-              2023/2/21
-            </Typography>
-          </Box>
-          <Box
-            width="100%"
-            py={2}
-            bgcolor="#F9FCFD"
-            borderRadius={2}
-            sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}
-          >
-            <Typography pl={2} sx={{ width: '33%' }} variant="h6">
-              [TIL]오늘 잠만 잤어요
-            </Typography>
-            <Typography sx={{ width: '33%', textAlign: 'center' }} variant="h6">
-              잠만보
-            </Typography>
-            <Typography sx={{ width: '33%', textAlign: 'center' }} variant="h6">
-              2023/2/21
-            </Typography>
-          </Box> */}
         </Stack>
       </Box>
     </Stack>
