9c7c1f7f861e35651f4c864f6aa9704ac6a28faa
GosuEE <leemin727@naver.com>
2022-12-13 15:32:47 +0900

    Add redux reducer, JSON server

--- src/redux/configure/configStore.jsx
+++ src/redux/configure/configStore.jsx
@@ -1,7 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { combineReducers } from 'redux';
+import text from '../modules/textSlice';
+import post from '../modules/postSlice';
 
-const rootReducer = combineReducers({});
+const rootReducer = combineReducers({
+  text,
+  post,
+});
 
 const store = configureStore({ reducer: rootReducer });
 
